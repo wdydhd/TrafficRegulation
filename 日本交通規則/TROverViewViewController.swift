27a827c7a6e124e3562dae9eabcdb1a886597175
YanQi <wdydhd@icloud.com>
2017-02-17 22:10:21 +0900

    ステータスバーが白くする

--- 日本交通規則/TROverViewViewController.swift
+++ 日本交通規則/TROverViewViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class TROverViewViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class TROverViewViewController: UIViewController{
+
     let dataDic:[(title : String , subTitle : String)] = [("左边行驶" , "日本的道路行驶，以靠左车道为准。") , ("右转车必须等待" , "原则上以直行车及左转车优先。\n右转车需等待逆向直行车通过后，再转弯通过。") , ("红灯必停" , "红灯时，不管是直行车或左转车，均需停车。") , ("遵守箭头绿灯的指示方向" , "遇上红灯，若有箭头绿灯时，表示仅准许车辆依箭头指示方向转弯行驶。") , ("注意行驶的速限，法定速限为时速60公里" , "无速限标志时，一般道路的速限为\n时速60公里．高速公路为时速100公里") , ("注意禁止超车" , "在标有禁止超车的\n仍被视为违反交通规则。且在平交道前，一定要停车再开。") , ("停车再开时需完全停止，遇上平交道时一定要停车再开" , "在日本需严格遵守减速暂时的动作。若不完全停止或缓行时，\n仍被视为违反交通规则。且在平交道前，一定要停车再开。") , ("切勿急踩油门前进，看清楚前方的号志" , "在拥挤车道的绿灯时间较长，以及在特定的十字路口设置\n有人车分离式的号志，分离人车动线，减少人车冲突。") , ("禁止酒后驾车" , "「酒后请勿驾驶」是绝对原则，搭乘喝酒驾驶者的车，\n或者是向驾驶人劝酒的人，都是违法的。")];
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +23,10 @@ class TROverViewViewController: UIViewController,UITableViewDelegate,UITableView
         // Dispose of any resources that can be recreated.
     }
     
+}
+
+extension TROverViewViewController : UITableViewDataSource,UITableViewDelegate {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataDic.count;
     }
