d5509b5e3516717b7c14f8da30f068eff3f42482
YanQi <wdydhd@icloud.com>
2017-02-22 22:00:53 +0900

    ベース交通規則表示画面追加

---
+++ 日本交通規則/TRBaseRuleIntroViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TRBaseRuleIntroViewController.swift
+//  日本交通規則
+//
+//  Created by YanQi on 2017/02/21.
+//  Copyright © 2017年 YanQi. All rights reserved.
+//
+
+import UIKit
+
+class TRBaseRuleIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 日本交通規則/TRConst.swift
@@ -0,0 +1,22 @@
+//
+//  TRConst.swift
+//  日本交通規則
+//
+//  Created by YanQi on 2017/02/21.
+//  Copyright © 2017年 YanQi. All rights reserved.
+//
+
+import Foundation
+
+
+enum TRSegueIdentifier: String {
+    case ToBaseRuleIntroSegue = "ToBaseRuleIntroSegue"
+}
+
+
+
+enum ASCIIControlCharacter: String {
+    case tab = "\t"
+    case lineFeed = "\n"
+    case carriageReturn = "\r"
+}
